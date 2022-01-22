7c672a422641b15cf156249cc1ecf07c34fe6b83
Ogino0423 <Ogino0423@github.com>
2022-01-22 21:38:54 +0900

    名前変更

--- client/App.tsx
+++ client/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/"}>
-            positronX.io
+            cncs 
           </Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
