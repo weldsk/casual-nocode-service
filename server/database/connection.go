bcba28115d917187caf260c2ba18357a172b2199
weldsk <>
2022-02-12 02:32:54 +0900

    デバッグ用にsqlliteを使用するよう追加

--- server/database/connection.go
+++ server/database/connection.go
@@ -3,19 +3,26 @@ package database
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func Connect(dbName string) (*gorm.DB, error) {
-	dbUser := os.Getenv("SERVER_USER")
-	dbPassword := os.Getenv("SERVER_PASS")
-	dbPath := os.Getenv("SERVER_DBPATH")
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		dbUser, dbPassword, dbPath, dbName,
-	)
+var DbUseLocal, _ = strconv.ParseBool(os.Getenv("SERVER_DB_LOCAL"))
+var DbUser = os.Getenv("SERVER_DBUSER")
+var DbPassword = os.Getenv("SERVER_DBPASS")
+var DbPath = os.Getenv("SERVER_DBPATH")
 
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func Connect(dbName string) (*gorm.DB, error) {
+	if DbUseLocal {
+		return gorm.Open(sqlite.Open("local.db"))
+	} else {
+		dsn := fmt.Sprintf(
+			"%s:%s@tcp(%s/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			DbUser, DbPassword, DbPath, dbName,
+		)
+		return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	}
 }
