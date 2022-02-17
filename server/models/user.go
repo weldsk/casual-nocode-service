7f9a288ed21923c6cc2be4fd1331273b4cc78f5d
weldsk <>
2022-02-18 01:06:18 +0900

    salt対応

--- server/models/user.go
+++ server/models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -8,6 +9,19 @@ type User struct {
 	gorm.Model
 	Name           string `json:"name"`
 	Email          string `json:"email"`
-	PasswordHashed string
-	PasswordSalt   string
+	PasswordHashed []byte
+}
+
+func (u *User) Login(password string) bool {
+	err := bcrypt.CompareHashAndPassword(u.PasswordHashed, []byte(password))
+	return err == nil
+}
+
+func (u *User) SetPassword(password string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	if err == nil {
+		u.PasswordHashed = hashed
+	}
+	return err
 }
