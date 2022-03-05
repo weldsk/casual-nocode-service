e7af4771b89edba7f378b55ddf3afca21e6ad658
kenji <kenji.0210.ku@gmail.com>
2022-03-05 21:10:53 +0900

    secretkeyを環境依存に変更

--- server/token/token.go
+++ server/token/token.go
@@ -2,6 +2,7 @@ package token
 
 import (
 	"casual-nocode-service/models"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -25,14 +26,23 @@ func CreateToken(user *models.User) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	return token.SignedString([]byte("secret_key"))
+	return token.SignedString([]byte(GetKey()))
 }
 
 func GetJwtConfig() middleware.JWTConfig {
-	return middleware.JWTConfig{
+	return middleware.JWgConfig{
 		Claims:     &jwtCustomClaims{},
-		SigningKey: []byte("secret_key"),
+		SigningKey: []byte(GetKey()),
 	}
+}
+
+func getKey() string {
+	key := os.Getenv("SECRET_KEY")
+	if key == "" {
+		panic("failed get key")
+	}
+
+	return key
 }
 
 func GetName(c echo.Context) string {
